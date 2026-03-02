c568aa6d6abaf800d70cb9821c54342c3c33964a
Nimesh Prajapati <nimprajapati2004@gmail.com>
2026-03-02 23:40:54 +0530

    Reverse word with character of string

--- ReverseString.java
+++ ReverseString.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ReverseString {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+
+        // Hello world
+        String reverseString = "";
+
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            reverseString += s1.charAt(i);
+        }
+
+        System.out.println("Original String : " + s1);
+        System.out.println("Reversed String : " + reverseString);
     }
 }
